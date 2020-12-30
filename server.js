8aeca71d08bf2e2bceb35f4c52357fcc3a77881b
Alexander Sanchez <pcua2014@hotmail.com>
2020-12-29 23:51:27 -0400

    URLs definition

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ const port = 3000;
 app.get('/',(req, res) => {
     // req es la request, la peticion
     // res es la respuesta
-    res.send('Hello World');
+    res.status(200).send('Hello World');
+});
+
+app.post('/team/pokemons', (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+app.get('/team', (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+app.delete('/team/pokemons/:pokeid', (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+app.put('/team', (req, res) => {
+    res.status(200).send('Hello World');
 });
 
 app.listen(port,() => {
