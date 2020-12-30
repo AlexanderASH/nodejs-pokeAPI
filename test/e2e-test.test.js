eef5c7514df6b9b7b99c7733139274ac8bf82141
Alexander Sanchez <pcua2014@hotmail.com>
2020-12-30 00:22:25 -0400

    test e2e with chai-http

---
+++ test/e2e-test.test.js
@@ -0,0 +1,20 @@
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const app = require('../server').app;
+
+describe('Suite de prueba e2e', () => {
+    it('should return Hello World', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                chai.assert.equal(res.text, 'Hello World');
+                //Soluciona problemas de asyncronia, avisamos cuando termina el test
+                done();
+            }
+        );
+    });
+});
